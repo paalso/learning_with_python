8dedcc14c80e53ed3850205cc242a895cfee840b
paalso <40a.pvl@gmail.com>
2020-02-03 18:08:14 +0200

    18 Recursion started. 18.1 done

---
+++ 18 Recursion/18-1.py
@@ -0,0 +1,47 @@
+﻿# Chapter 18. Recursion
+# http://openbookproject.net/thinkcs/python/english3e/recursion.html
+
+# Exercise 1
+# ===========
+# Modify the Koch fractal program so that it draws a Koch snowflake
+
+
+import turtle
+import turtle_helper
+
+
+def koch(t, order, size):
+    """
+       Make turtle t draw a Koch fractal of 'order' and 'size'.
+       Leave the turtle facing the same direction.
+    """
+    if order == 0:
+        t.forward(size)
+        return
+
+    for turn in (60, -120, 60, 0):
+        koch(t, order - 1, size // 3)
+        t.left(turn)
+
+
+def koch_snowflake(t, order, size):
+    """
+       Make turtle t draw a Koch fractal snowflake of 'order' and 'size'.
+    """
+    for i in range(3):
+        koch(t, order, size)
+        t.right(120)
+
+
+def main():
+    wn = turtle_helper.make_window("lightgreen", "Koch's curve")
+    t = turtle_helper.make_turtle("blue", 2, -200, 100)
+    t.speed(0)
+
+    koch_snowflake(t, 2, 250)
+
+    wn.mainloop()
+
+
+if __name__ == '__main__':
+    main()
---
+++ 18 Recursion/koch.py
@@ -0,0 +1,31 @@
+﻿import turtle
+import turtle_helper
+
+
+def koch(t, order, size):
+    """
+       Make turtle t draw a Koch fractal of 'order' and 'size'.
+       Leave the turtle facing the same direction.
+    """
+    if order == 0:
+        t.forward(size)
+        return
+
+    for turn in (60, -120, 60, 0):
+        koch(t, order - 1, size // 3)
+        t.left(turn)
+
+
+def main():
+    wn = turtle_helper.make_window("lightgreen", "Koch's curve")
+    t = turtle_helper.make_turtle("blue", 2, -250, 0)
+    t.speed(0)
+
+    koch(t, 4, 600)
+
+    wn.mainloop()
+
+
+if __name__ == '__main__':
+    main()
+
