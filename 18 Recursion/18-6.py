48fb500b72abc284101dc6cc517b0113e2d02ba0
paalso <40a.pvl@gmail.com>
2020-02-15 17:55:36 +0200

    18: 6, 7

---
+++ 18 Recursion/18-6.py
@@ -0,0 +1,37 @@
+﻿# Chapter 18. Recursion
+# http://openbookproject.net/thinkcs/python/english3e/recursion.html
+
+# Exercise 6
+# ===========
+# Write a function count that returns the number of occurrences of target
+# in a nested list
+
+from testtools import test
+
+
+def count(target, nxc):
+    """
+       Returns the number of occurrences of target in a nested list
+    """
+    counter = 0
+    for e in nxc:
+        if type(e) == type([]):
+            counter += count(target, e)
+        else:
+            if e == target:
+                counter += 1
+    return counter
+
+
+def main():
+    test(count(2, []) == 0)
+    test(count(2, [2, 9, [2, 1, 13, 2], 8, [2, 6]]) == 4)
+    test(count(7, [[9, [7, 1, 13, 2], 8], [7, 6]]) == 2)
+    test(count(15, [[9, [7, 1, 13, 2], 8], [2, 6]]) == 0)
+    test(count(5, [[5, [5, [1, 5], 5], 5], [5, 6]]) == 6)
+    test(count("a",
+     [["this",["a",["thing","a"],"a"],"is"], ["a","easy"]]) == 4)
+
+
+if __name__ == '__main__':
+    main()
---
+++ 18 Recursion/18-7.py
@@ -0,0 +1,36 @@
+﻿# Chapter 18. Recursion
+# http://openbookproject.net/thinkcs/python/english3e/recursion.html
+
+# Exercise 7
+# ===========
+# Write a function flatten that returns a simple list containing all the values
+# in a nested list
+
+from testtools import test
+
+
+def flatten(nxc):
+    """
+       Returns a simple list containing all the values in a nested lis
+    """
+    flattenned = []
+    for e in nxc:
+        if type(e) == type([]):
+            flattenned.extend(flatten(e))
+        else:
+            flattenned.append(e)
+
+    return flattenned
+
+
+def main():
+    test(flatten([2,9,[2,1,13,2],8,[2,6]]) == [2,9,2,1,13,2,8,2,6])
+    test(flatten([[9,[7,1,13,2],8],[7,6]]) == [9,7,1,13,2,8,7,6])
+    test(flatten([[9,[7,1,13,2],8],[2,6]]) == [9,7,1,13,2,8,2,6])
+    test(flatten([["this",["a",["thing"],"a"],"is"],["a","easy"]]) ==
+                  ["this","a","thing","a","is","a","easy"])
+    test(flatten([]) == [])
+
+
+if __name__ == '__main__':
+    main()
