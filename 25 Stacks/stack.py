3273182df0084818901d0a2f4df334a08b394eab
paalso <40a.pvl@gmail.com>
2020-04-03 10:52:32 +0300

    Chapter 25. Stacks. My version of postfix expressions evaluator - more advanced, but still is not enough satisfactory

--- 25 Stacks/stack.py
+++ 25 Stacks/stack.py
@@ -15,24 +15,45 @@ class Stack:
         self.size += 1
 
     def pop(self):
-        if self.size > 0: self.size -= 1
-        return self.items.pop()
+        if self.size > 0:
+            self.size -= 1
+            return self.items.pop()
 
     def is_empty(self):
         return self.size == 0
 
 
 def eval_postfix(expr):
-    import re
+    import re, operator
+
+    bin_operations = {
+        '+' : operator.add,
+        '-' : operator.sub,
+        '*' : operator.mul,
+        '/' : operator.truediv
+    }
+
     token_list = re.split("([^0-9])", expr)
+    expr_stack = Stack()
+    print(token_list)
+    for token in token_list:
+        if token == '' or token == ' ':
+            continue
+        elif token in bin_operations:
+                operand2 = expr_stack.pop()
+                operand1 = expr_stack.pop()
+                expr_stack.push(bin_operations[token](operand1, operand2))
+        else:       # token is a number
+            expr_stack.push(int(token))
+
+    return expr_stack.pop()
 
 
 def main():
-    pass
+    expr = '5 4 2 1 / + *'
+    expr = '1 2 + 3 *'
+    print(eval_postfix(expr))
+
 
 if __name__ == "__main__":
     main()
-
-
-
-
