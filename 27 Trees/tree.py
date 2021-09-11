38fedac5c26a0b55a99bbb4dbf8b09477851b83f
paalso <40a.pvl@gmail.com>
2021-09-11 11:34:26 +0300

    Tree: expression parsing problem: begin

--- 27 Trees/tree.py
+++ 27 Trees/tree.py
@@ -22,47 +22,57 @@ def total(tree):
     return value + total(tree.left) + total(tree.right)
 
 
-def print_tree(tree):
-    if tree is None: return
-    print(tree.cargo, end=" ")
-    print_tree(tree.left)
-    print_tree(tree.right)
+def print_tree_preorder(tree):
+    if tree is None:
+        return
+    print(tree.cargo, end=' ')
+    print_tree_preorder(tree.left)
+    print_tree_preorder(tree.right)
 
 
 def print_tree_postorder(tree):
-    if tree is None: return
+    if tree is None:
+        return
     print_tree_postorder(tree.left)
     print_tree_postorder(tree.right)
-    print(tree.cargo, end=" ")
+    print(tree.cargo, end=' ')
 
 
 def print_tree_inorder(tree):
-    if tree is None: return
+    if tree is None:
+        return
     print_tree_inorder(tree.left)
-    print(tree.cargo, end=" ")
+    print(tree.cargo, end=' ')
     print_tree_inorder(tree.right)
 
 
+# 3 * 2 + (5 + (4 - 1) * 2)
+expr_tree = \
+    Tree('+', Tree('*', Tree(3), Tree(2)), Tree('+', Tree(5), Tree('*', Tree('-', Tree(4), Tree(1)), Tree(2))))
+
+# 1 + 2 * 3
+expr_tree = Tree('+', Tree(1), Tree('*', Tree(2), Tree(3)))
+
+print()
+print("Preorder printing of the tree:")
+print_tree_preorder(expr_tree)
+
+print()
+print("Postorder printing of the tree")
+print_tree_postorder(expr_tree)
+
+print()
+print("Inorder printing of the tree")
+print_tree_inorder(expr_tree)
+
+
 def print_tree_indented(tree, level=0):
-    if tree is None: return
+    if tree is None:
+        return
     print_tree_indented(tree.left, level + 1)
-    print(" " * level, end="")
-    print(tree.cargo)
+    print('  ' * level + str(tree.cargo))
     print_tree_indented(tree.right, level + 1)
 
-
-tree = Tree("+", Tree(1), Tree("*", Tree(2), Tree(3)))
-print_tree(tree)    # + 1 * 2 3
 print()
-print_tree_postorder(tree)  # 1 2 3 * +
-print()
-print_tree_inorder(tree)  # 1 + 2 * 3
-print()
-print_tree_indented(tree)
-"""
-    3
-  *
-    2
-+
-  1
-"""
+print("Indented inorder printing of the tree")
+print_tree_indented(expr_tree)
